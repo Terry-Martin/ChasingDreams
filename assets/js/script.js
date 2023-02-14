17bfc5a09766e72addb58236f99cf23d67542b48
Terry-Martin <terry.martin@gmail.com>
2023-02-14 23:15:18 +0000

    Add Character object with test properites and method

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,26 @@
+const character = {
+    name: "Terry",
+    eyeColour: "blue",
+    hairColour: "brown",
+    relaxLevel: 100,
+    tiredness: 132,
+    mood: function () {
+        return (this.relaxLevel + this.tiredness) / 2;
+    }
+};
+
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 
 document.addEventListener("DOMContentLoaded", function () {
     throw ("Alert Works using javascript to console");
 })
+
+console.log(character.name);
+console.log(character.mood());
+
+//let x = character.mood();
+//document.getElementById("currentMood").textContent = x;
+
+
+document.getElementById("currentMood").textContent = character.mood();
