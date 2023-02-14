d938ff54f753adc41837d3e4bd3312d6a30ad17b
Terry-Martin <terry.martin@gmail.com>
2023-02-14 16:28:22 +0000

    Add placeholder text and lists for each section in html

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function () {
+    throw ("Alert Works using javascript to console");
+})
