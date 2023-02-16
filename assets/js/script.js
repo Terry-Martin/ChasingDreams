5bc116bfc2f0256111ea75b3de11c6cae0ee04a0
Terry-Martin <terry.martin@gmail.com>
2023-02-16 09:51:36 +0000

    Style added to class using javascript. General testing.

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,9 @@ const character = {
     }
 };
 
+
+
+
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 
@@ -24,3 +27,38 @@ console.log(character.mood());
 
 
 document.getElementById("currentMood").textContent = character.mood();
+
+
+// Set weather array and display random weather data
+const weather = ["Raining", "Sunny", "Snowy", "Hurricane"];
+let weatherType = Math.floor(Math.random() * weather.length);
+document.getElementById("weather").textContent = "Weather: " + weather[weatherType];
+
+// Set terrain array and display random terrain data
+//https://outforia.com/types-of-terrain/
+const terrain = ["Foothills", "Swamp", "Meadow", "Forest", "Canyon", "Valley", "Dunes", "Glacier"];
+let terrainType = Math.floor(Math.random() * terrain.length);
+document.getElementById("terrain").textContent = "Terrain: " + terrain[terrainType];
+
+document.getElementById("terrain").style.fontSize = "35px";
+document.getElementById("terrain").style.color = "blue";
+
+// hide element/text
+//document.getElementById("terrain").style.display = "none";
+
+
+// alert example
+//window.alert(5 + 6);
+//alert(5 + 6);
+
+// Returns a random integer from 0 to 99:
+Math.floor(Math.random() * 100);
+
+
+
+// Edit style using getElementsByClassName
+for (const s of document.getElementsByClassName("player-stats")) {
+    s.style.color = "green";
+}
+
+document.getElementsByClassName("player-stats")[2].style.color = "yellow";
